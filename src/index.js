61ac793a1c0008b6a8fe72b1581b1d9be6238a9a
lUCI <lusindisontanjana@gmail.com>
2022-04-26 09:47:50 +0200

    Add js function to diplay to do list dynamical Add css styles to layout the page Add MIT license file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './styles/main.scss';
+
+const todoList = document.querySelector('.todo-list');
+
+const todo = [
+  {
+    description: 'Wash dishes',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Complete To Do project',
+    completed: false,
+    index: 0,
+  },
+];
+
+const createList = () => {
+  todo.forEach((el) => {
+    const li = document.createElement('li');
+    li.innerText = el.description;
+    todoList.appendChild(li);
+  });
+};
+
+createList();
