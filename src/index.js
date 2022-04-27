454b44d7a365ee4774cb889af684de57ed21d578
lUCI <lusindisontanjana@gmail.com>
2022-04-27 12:11:35 +0200

    Add js functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-import handleSubmit from './functionality.js';
+import handleSubmit from './modules/handleSubmit.js';
 import './styles/main.scss';
 
-const todoList = document.querySelector('.todo-list');
-
-const task = [];
-handleSubmit(task);
-
-const createList = () => {
-  task.forEach((el) => {
-    const li = document.createElement('li');
-    li.innerHTML = `<span><i class="fa-regular fa-square"></i> ${el.description}</span><i class="fa-solid fa-ellipsis-vertical"></i>`;
-    todoList.appendChild(li);
-  });
-};
-
-createList();
+const tasks = [];
+handleSubmit(tasks);
